e1c4495d084a8f8a21921a26788a4b559cf705aa
Murtazin <renatmurtazin@yahoo.com>
2022-05-27 21:57:45 +0300

    Identity funcs addition

--- MyBook.WEB/Program.cs
+++ MyBook.WEB/Program.cs
@@ -5,6 +5,9 @@ using MyBook.WEB.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();;
+
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
