4c11ede3aceb5a3f12968fe9e0b8a5caac64fbae
Murtazin <renatmurtazin@yahoo.com>
2022-06-10 17:35:14 +0300

    some changes

--- MyBook.DAL/MyBook.DAL/Entities/Subscription.cs
+++ MyBook.DAL/MyBook.DAL/Entities/Subscription.cs
@@ -3,7 +3,7 @@
 public class Subscription
 {
     public int Id { get; set; }
-    public decimal Price { get; set; }
+    public int Price { get; set; }
     public string Name { get; set; } = null!;
     public int Duration { get; set; } //minutes ??
     public string Description { get; set; } = null!;
